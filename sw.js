3d0870f4ab068de635efe495974ff9a932bf9ab4
M7-netizen <meiqi990414@gmail.com>
2026-05-19 21:10:53 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yuezh-v56';
+const CACHE = 'yuezh-v57';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/header-logo.png'];
 
 // Install: cache core files
